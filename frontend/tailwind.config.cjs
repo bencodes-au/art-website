33ed2c5f096e10a3c80a835488faa8a60da041d1
Ben Gorman <bencodes.au@gmail.com>
2025-12-11 06:06:53 +1100

    fix: full stack package lock issues

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,10 +3,8 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [require("daisyui")],
 };
